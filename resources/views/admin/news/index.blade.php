f1d322a0bb5990dca316d9e96c9730b627ff80b5
髙藤竜平 <jkkrs1221185@gmail.com>
2021-03-07 15:26:23 +0000

    Laravel 16 修正後

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -26,7 +26,7 @@
             </div>
         </div>
         <div class="row">
-            <div class="list-news col-md-12 mx-auto">
+            <div class="admin-news col-md-12 mx-auto">
                 <div class="row">
                     <table class="table table-dark">
                         <thead>
@@ -45,7 +45,10 @@
                                     <td>{{ str_limit($news->body, 250) }}</td>
                                     <td>
                                         <div>
-                                            <a href="{{ action('Admin\NewsController@edit', ['id' => $news->id] }}">編集</a>
+                                            <a href="{{ action('Admin\NewsController@edit', ['id' => $news->id]) }}">編集</a>
+                                        </div>
+                                        <div>
+                                            <a href="{{ action('Admin\NewsController@delete', ['id' => $news->id]) }}">削除</a>
                                         </div>
                                     </td>
                                 </tr>
