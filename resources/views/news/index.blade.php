4b147d7d4eab61cf2d7c2ce7c2cdfa07b114f8d4
髙藤竜平 <jkkrs1221185@gmail.com>
2021-03-15 06:43:19 +0000

    トップページに「最近のニュース一覧」（中央揃え、文字色：青）追加

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
     <div class="container">
+         <div class="text-center">
+            <h2>最近のニュース一覧</h2>
+        </div>
         <hr color="#c0c0c0">
         @if (!is_null($headline))
             <div class="row">
